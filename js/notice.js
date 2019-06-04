fa42e6818cc98dfed26a9859e017f47a10323eea
root <root@srf2-integration.bpi.fr>
2019-06-04 15:43:02 +0200

    Meme themes js

--- js/notice.js
+++ js/notice.js
@@ -3,13 +3,13 @@ function sleep(ms)
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function loadIndexes()
+async function loadMemeThemes()
 {
 	
-	var feuilletageIndexesLists = document.getElementById("datasIndexes");
-	var indiceCdu = feuilletageIndexesLists.getElementsByClassName("indiceCduFeuille")[0].innerHTML;
-	var isOnline = feuilletageIndexesLists.getElementsByClassName("isOnlineFeuille")[0].innerHTML;
-	var tabDiv = feuilletageIndexesLists.getElementsByClassName("feuilletageIndexes-tab")[0];
+	var memeThemesLists = document.getElementById("datasIndexes");
+	var indiceCdu = memeThemesLists.getElementsByClassName("indiceCduFeuille")[0].innerHTML;
+	var isOnline = memeThemesLists.getElementsByClassName("isOnlineFeuille")[0].innerHTML;
+	var tabDiv = memeThemesLists.getElementsByClassName("feuilletageIndexes-tab")[0];
 	tabDiv.innerHTML = "<img style=\"width:60px; height: 60px;\" src=\"/img/spin.svg\" alt=\"loading\" srcset=\"/img/spin.svg\"/>";
 
 	await sleep(750);
@@ -22,7 +22,7 @@ async function loadIndexes()
 
 $(window).load(function()
 {
-	loadIndexes();
+	loadMemeThemes();
 	//loadQuatrieme();
 	//loadTableMatieres();
 });
